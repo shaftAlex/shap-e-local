e0a0ab6bad4ce7ba9e181f5867f1c5e6246ac82d
shaftAlex <alexanderscriminger@gmail.com>
2023-09-19 10:14:18 -0400

    Startup.bat
    Pulls most recent git changes, activates the python venv, and opens visual studio code with the project folder open

---
+++ startup.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Check for changes in the Git repository
+git pull
+
+REM Activate the virtual environment (replace 'myenv' with your environment name)
+call venv\Scripts\activate
+
+REM Open Visual Studio Code and navigate to the project folder
+code .
